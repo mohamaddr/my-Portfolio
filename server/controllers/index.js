f6bed8347237557e84ea7786f39c33d750b527b5
mohamaddr <gusdrgmo@student.gu.se>
2020-09-04 16:22:32 +0200

    contact me

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,12 @@
 var path = require("path");
 var express = require("express");
 var router = express.Router();
-var massagesController = require("./massages");
-var sendController = require("./send");
 
 router.get("/api", function (req, res) {
   res.json({ message: "Welcome to your DIT341 backend project!" });
 });
 
 // Insert routes below
-router.use("/api/massages", massagesController);
-router.use("/api/send", sendController);
 
 // All other routes redirect to the index.html
 router.route("/pro").get(function (req, res) {
